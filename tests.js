1d40a3669b2eec94e46363de867a0a5f386e2ebc
Rodriques <rodriques.perry@gmail.com>
2021-08-24 12:02:55 -0500

    unit tests for edge cases.

--- tests.js
+++ tests.js
@@ -25,25 +25,25 @@ describe('sayHello', function () {
     it('should return the string "Hello, Jane!" when executed' , function () {
         expect(sayHello('Jane')).toBe('Hello, Jane!');
     });
-    it('should return the srting "Hello, Alex!" when executed', function () {
+    it('should return the string "Hello, Alex!" when executed', function () {
         expect(sayHello('Alex')).toBe('Hello, Alex!');
     });
-    it('should return the srting "Hello, Pat!" when executed', function () {
+    it('should return the string "Hello, Pat!" when executed', function () {
         expect(sayHello('Pat')).toBe('Hello, Pat!');
     });
-    it('should return the srting "Hello, World!" when executed', function () {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(sayHello()).toBe('Hello, World!');
     });
-    it('should return the srting "Hello, World!" when executed', function () {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(sayHello(true)).toBe('Hello, World!');
     });
-    it('should return the srting "Hello, World!" when executed', function () {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(sayHello(false)).toBe('Hello, World!');
     });
-    it('should return the srting "Hello, World!" when executed', function () {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(sayHello(null)).toBe('Hello, World!');
     });
-    it('should return the srting "Hello, World!" when executed', function () {
+    it('should return the string "Hello, World!" when executed', function () {
         expect(sayHello("")).toBe('Hello, World!');
     });
 })
