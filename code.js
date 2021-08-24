2c2626d4d2ffaf570a296e91ea1c1f42bf679991
Rodriques <rodriques.perry@gmail.com>
2021-08-24 11:11:46 -0500

    complete: add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+
+// // function expression syntax (assigning an anonymous function to a variable)
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+
+// sayHello function
+
+const sayHello = () => {
+
 }
--- tests.js
+++ tests.js
@@ -1,3 +1,4 @@
+
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
@@ -13,3 +14,9 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    })
+})
